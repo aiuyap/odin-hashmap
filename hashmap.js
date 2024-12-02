84de6ebcb32c9edf3dbae41679310cb52b448153
aiuyap <186591613+aiuyap@users.noreply.github.com>
2024-12-03 00:56:47 +0800

    Created additional functions -set -get -has -remove -length -clear -keys

--- hashmap.js
+++ hashmap.js
@@ -1,17 +1,17 @@
 function HashMap() {
   let capacity = 16;
   let loadFactor = 0.75;
-  let buckets = [];
+  let buckets = new Array(capacity);
 
   function hash(key) {
     let hashCode = 0;
 
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % capcity;
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % capacity;
     }
 
-    return hashCode;
+    return hashCode % capacity;
   }
 
   function set(key, value) {
@@ -21,10 +21,65 @@ function HashMap() {
       throw new Error("Trying to access index out of bounds");
     }
 
-    if (buckets[index].key === key) {
+    if (buckets[index]) {
       buckets[index].value = value;
     }
 
     buckets[index] = { key, value };
   }
+
+  function get(key) {
+    const index = hash(key);
+
+    if (!buckets[index]) {
+      return null;
+    }
+    return buckets[index].value;
+  }
+
+  function has(key) {
+    const index = hash(key);
+
+    if (!buckets[index]) {
+      return false;
+    }
+    return true;
+  }
+
+  function remove(key) {
+    const index = hash(key);
+
+    if (!buckets[index]) {
+      return false;
+    }
+    buckets.splice(index);
+    return true;
+  }
+
+  function length() {
+    let count = 0;
+    buckets.forEach((item) => {
+      if (item) count++;
+    });
+    return count;
+  }
+
+  function clear() {
+    buckets = new Array(capacity);
+  }
+
+  function keys() {
+    let keys = [];
+    buckets.forEach((item) => {
+      keys.push(item.key);
+    });
+    return keys;
+  }
+
+  return { set, get, has, remove, length, clear, keys };
 }
+
+const myMap = HashMap();
+myMap.set("apple", "red");
+console.log(myMap.length());
+console.log(myMap.keys());
